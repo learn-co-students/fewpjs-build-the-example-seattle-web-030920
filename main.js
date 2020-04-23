e4cf8e82360a845b21731e771229142cbd14dea0
ShunshunL <shunshunlyu@protonmail.com>
2020-04-22 19:24:07 -0700

    Done.

--- main.js
+++ main.js
@@ -2,10 +2,37 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
+let glyphStates = {
+  '♡': '♥',
+  '♥': '♡'
+}
+
+let colorStates = {
+  'red': '',
+  '': 'red'
+}
+
+let heartBanner = document.querySelectorAll('.like')
+
+
 // Your JavaScript code goes here!
+let articleHearts = document.querySelectorAll(".like");
 
+function likeCallback(e) {
+  let heart = e.target;
+  mimicServerCall("bogusUrl")
+    .then(function(serverMessage){
+       heart.innerText = glyphStates[heart.innerText];
+       heart.style.color = colorStates[heart.style.color];
+    })
+    .catch(function(error) {
+      document.getElementById("modal").className = "";
+    });
+}
 
-
+for (let glyph of articleHearts) {
+  glyph.addEventListener("click", likeCallback);
+}
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
